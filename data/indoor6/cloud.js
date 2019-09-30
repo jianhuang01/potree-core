530386e013a07329189f7df0f1c1caf8f66c6408
Jian Huang <jianh@uber.com>
2019-09-30 14:59:59 -0700

    scale up to 0.1

---
+++ data/indoor6/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8069039,
+    "boundingBox": {
+        "lx": -23.135099411010743,
+        "ly": -2.005009889602661,
+        "lz": -8.808819770812989,
+        "ux": 5.65811014175415,
+        "uy": 26.78819966316223,
+        "uz": 19.984389781951906
+    },
+    "tightBoundingBox": {
+        "lx": -23.14,
+        "ly": -2.0100000000000004,
+        "lz": -8.81,
+        "ux": 5.66,
+        "uy": 7.57,
+        "uz": 12.36
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "INTENSITY"
+    ],
+    "spacing": 0.24935650825500489,
+    "scale": 0.1,
+    "hierarchyStepSize": 5
+}
